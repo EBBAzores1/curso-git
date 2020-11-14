c88e4d1a232d5b746cb28ade55ba1128a772f1e0
Paulo Costa <pauloandre.c.costa@gmail.com>
2020-11-14 00:06:31 +0000

    Projecto iniciado

---
+++ ProjectoTeste/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectoTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
